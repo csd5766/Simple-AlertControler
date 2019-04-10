f492e10a55c602e348ba3b1372ffc96f787b8479
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-04-10 16:43:56 +0900

    Simple Alert

--- Simple AlertControler/ViewController.swift
+++ Simple AlertControler/ViewController.swift
@@ -10,23 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
-        let alertController = UIAlertController(title: "카메라", message: "Take a picture!", preferredStyle: .actionSheet)
+        let alertController = UIAlertController(title: "카메라", message: "Take a picture!", preferredStyle: .alert)
         let camAction = UIAlertAction(title: "cam title", style: .default, handler: nil)
         
         let libAction = UIAlertAction(title: "Library", style: .default, handler: nil)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
-        let delAction = UIAlertAction(title: "Delete", style: .destructive, handler: nil)
-        
+       
         let saveAction = UIAlertAction(title: "svae", style: .default, handler: nil)
         
+//        let delAction = UIAlertAction(title: "Delete", style: .default, handler: {(action: UIAlertAction) -> Void in self.view.backgroundColor = UIColor.blue
+//
+//
+//        })
+        
+        //후행 클로저(trailing closure)
+        
+        let delAction = UIAlertAction(title: "Delete", style: .default)
+        {(action: UIAlertAction) -> Void in self.myLabel.text = "dddddd"
+        }
         
         alertController.addAction(camAction)
         alertController.addAction(cancelAction)
